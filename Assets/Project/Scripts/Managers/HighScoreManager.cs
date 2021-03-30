4ee6d2d85e7897b89468bcbf044ae29df1ebee0f
V4lonforth <v4lonforth@gmail.com>
2021-03-30 16:08:04 +0300

    Implement high score

---
+++ Assets/Project/Scripts/Managers/HighScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts.Managers
+{
+    public class HighScoreManager : MonoBehaviour
+    {
+        public Action<int> OnHighScoreChange { get; set; }
+        
+        [SerializeField] private ScoreManager scoreManager;
+
+        private int _highScore;
+
+        private const string HighScoreName = "HighScore";
+        
+        private void Awake()
+        {
+            GameManager.Instance.OnGameFinish += CheckHighScore;
+        }
+
+        private void Start()
+        {
+            _highScore = GetHighScore();
+            OnHighScoreChange?.Invoke(_highScore);
+        }
+
+        private void CheckHighScore()
+        {
+            if (scoreManager.Score <= _highScore) return;
+            
+            _highScore = scoreManager.Score;
+            SaveHighScore(_highScore);
+            OnHighScoreChange?.Invoke(_highScore);
+        }
+
+        private int GetHighScore()
+        {
+            return PlayerPrefs.HasKey(HighScoreName) ? PlayerPrefs.GetInt(HighScoreName) : 0;
+        }
+
+        private void SaveHighScore(int highScore)
+        {
+            PlayerPrefs.SetInt(HighScoreName, highScore);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UI/HighScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using Scripts.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts.UI
+{
+    public class HighScoreDisplay : MonoBehaviour
+    {
+        [SerializeField] private Text highScoreText;
+
+        private void Awake()
+        {
+            var highScoreManager = FindObjectOfType<HighScoreManager>();
+            
+            highScoreManager.OnHighScoreChange += UpdateText;
+        }
+
+        private void UpdateText(int score)
+        {
+            highScoreText.text = score.ToString();
+        }
+    }
+}
